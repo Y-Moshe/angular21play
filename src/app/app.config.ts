c545af09d1b36349572abe02b57a6a3fba42e9b8
Y-Moshe <moshe.nehemiah@gmail.com>
2026-04-27 19:03:50 +0300

    feat(routes): add app.routes.ts for application routing configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth';
-import { routes } from './routes/app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
